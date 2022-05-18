e49b64be857e60d677635d80f41d24009113482b
Callum Hopkins <103919468@student.swin.edu.au>
2022-05-18 16:13:58 +1000

    Completed all tasks

--- Task/arrow-functions.js
+++ Task/arrow-functions.js
@@ -7,3 +7,57 @@
 //addTwoNums(5, 10);
 
 //Q3
+//Create a function to calculate the sum of elements in an array.
+/*let sumArray = () => {
+    let nums = [];
+    alert("Input 5 numbers");
+
+    for(let i = 0; i < 5; i++)
+    {
+    nums[i] = parseInt(prompt(`element - ${i} : `));
+    }
+
+    let arrSum = nums.reduce((a, b) => a + b);
+    console.log(arrSum);
+}
+sumArray();*/
+
+//Q4
+//Create a function to calculate the result 
+//of raising an integer number to another. 
+/*let raised = (n1, n2) => {
+n1 = prompt("Input base number:");
+n2 = prompt("Input the exponent:")
+console.log(Math.pow(n1, n2));
+}
+raised();*/
+
+//Q5
+//Create a function to calculate the 
+//Fibonacci number of a specific term
+let fib = () => {
+    let first = 0;
+    let second = 1;
+
+    let sequence = [];
+
+    let counter = parseInt(prompt("Enter a number:"));
+
+    if(counter >= 1)
+    {
+        sequence.push(0);
+    } else if (counter >= 2)
+    {
+        sequence.push(1);
+    }
+
+    for(let i = 2; i <= counter; i++)
+    {
+        let oneTwo = first + second;
+        first = second;
+        second = oneTwo;
+        sequence.push(oneTwo);
+    }
+    console.log(sequence[counter - 1]);
+}
+fib();
